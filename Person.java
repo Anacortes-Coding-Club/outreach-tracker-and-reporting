dea97c6c0dae6e4e55eca6ea0c0abf14b9694be5
KaiG1024 <118638427+KaiG1024@users.noreply.github.com>
2023-03-09 14:41:50 -0800

    Delete Person.java

--- Person.java
+++
@@ -1,9 +0,0 @@
-public class Person{
-    public static ArrayList<String> names = new ArrayList<String>();
-    String name = null;
-    String 
-    public Person(String a){
-        name = a;
-        names.add(a);
-    }
-}
