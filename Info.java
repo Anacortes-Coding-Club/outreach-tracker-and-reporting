e47f0e38906394957009406c40417be1238cce26
KaiG1024 <118638427+KaiG1024@users.noreply.github.com>
2023-03-09 14:40:57 -0800

    Delete Info.java

--- Info.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class Info{
-    public static ArrayList<Visit> visits = new ArrayList<Visit>();
-    public ArrayList<Person> people = new ArrayList<Person>();
-    
-}
